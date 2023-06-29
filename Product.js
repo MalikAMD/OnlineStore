8d4df465932aae27f8beeaadb04686a0fc203574
Malik <malik.amadou89@gmail.com>
2023-06-29 20:36:35 +0200

    Création de la classe product visant à créer un produit à l'aide de id, name, category, price, quantity

---
+++ Product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, name, category, price, quantity) {
+      this.id = id;
+      this.name = name;
+      this.category = category;
+      this.price = price;
+      this.quantity = quantity;
+    }
+  }
+  module.exports = Product
