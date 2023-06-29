f231693343d382c35d273eab5cc8db492e5aab2e
Malik <malik.amadou89@gmail.com>
2023-06-29 20:34:39 +0200

    Création de la classe customers visant à créer un client à l'aide de son id, name, email et son historique d'achat

---
+++ Customers.js
@@ -0,0 +1,9 @@
+class Customer {
+    constructor(id, name, email, purchaseHistory) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.purchaseHistory = purchaseHistory;
+    }
+  }
+  module.exports = Customer
